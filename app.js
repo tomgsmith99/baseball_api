e61e4a2e1b0fee90082b2a6c1c99d1233ee0cf6d
Tom Smith <tomgsmith99@gmail.com>
2023-04-08 21:29:29 -0400

    2023 updates

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('/api/owners/:owner_id/current', (req, res) => {
 
 	const owner_id = req.params.owner_id
 
-	let query = `SELECT bank, owner_id, nickname, season FROM ownersXseasons_detail WHERE season = ${season} AND owner_id = ${owner_id}`
+	let query = `SELECT bank, owner_id, nickname, season FROM owner_x_season_detail WHERE season = ${season} AND owner_id = ${owner_id}`
 
 	console.log(query)
 
@@ -85,8 +85,6 @@ app.get('/api/owners/:owner_id/current/trades/count', (req, res) => {
 
 	const owner_id = req.params.owner_id
 
-	// let query = `SELECT COUNT(player_id) AS roster_count FROM ownersXrosters_current WHERE owner_id=${owner_id}`
-
 	let query = `SELECT COUNT(player_id) AS trades FROM owner_x_roster_detail WHERE owner_id=${owner_id} AND season = ${season} AND benched != 0`
 
 	console.log(query)
@@ -110,33 +108,33 @@ app.get('/api/owners/:owner_id/current/trades/count', (req, res) => {
 	})
 })
 
-app.get('/api/owners/:owner_id/players/:player_id', (req, res) => {
+// app.get('/api/owners/:owner_id/players/:player_id', (req, res) => {
 
-	const owner_id = req.params.owner_id
-	const player_id = req.params.player_id
+// 	const owner_id = req.params.owner_id
+// 	const player_id = req.params.player_id
 
-	let query = `INSERT INTO ownersXrosters_current (owner_id, player_id, season, drafted)`
+// 	let query = `INSERT INTO ownersXrosters_current (owner_id, player_id, season, drafted)`
 
-	query += ` VALUES (${owner_id}, ${player_id}, ${season}, 1)`
+// 	query += ` VALUES (${owner_id}, ${player_id}, ${season}, 1)`
 
-	console.log(query)
+// 	console.log(query)
 
-	var connection = dbconn.mysql_conn()
+// 	var connection = dbconn.mysql_conn()
 
-	connection.query(query, function (err, data) {
-		if (err) {
-			console.log(err)
-			res.json(err)
-			return
-		}
+// 	connection.query(query, function (err, data) {
+// 		if (err) {
+// 			console.log(err)
+// 			res.json(err)
+// 			return
+// 		}
 
-		res.json({"status": "ok"})
+// 		res.json({"status": "ok"})
 
-		connection.end(function(err) {
-			console.log("terminated mysql connection.")
-		})
-	})
-})
+// 		connection.end(function(err) {
+// 			console.log("terminated mysql connection.")
+// 		})
+// 	})
+// })
 
 app.get('/api/owners/:owner_id/team/starters', (req, res) => {
 
@@ -162,80 +160,80 @@ app.get('/api/owners/:owner_id/team/starters', (req, res) => {
 	})
 })
 
-app.get('/api/owners/:owner_id/team_name', (req, res) => {
+// app.get('/api/owners/:owner_id/team_name', (req, res) => {
 
-	const owner_id = req.params.owner_id
+// 	const owner_id = req.params.owner_id
 
-	let query = `SELECT team_name FROM ownersXseasons_all_time WHERE owner_id=${owner_id} ORDER BY season DESC LIMIT 1`
+// 	let query = `SELECT team_name FROM ownersXseasons_all_time WHERE owner_id=${owner_id} ORDER BY season DESC LIMIT 1`
 
-	console.log(query)
+// 	console.log(query)
 
-	var connection = dbconn.mysql_conn()
+// 	var connection = dbconn.mysql_conn()
 
-	connection.query(query, function (err, data) {
-		if (err) {
-			console.log(err)
-			res.json(err)
-			return
-		}
-		res.json(data[0])
+// 	connection.query(query, function (err, data) {
+// 		if (err) {
+// 			console.log(err)
+// 			res.json(err)
+// 			return
+// 		}
+// 		res.json(data[0])
 
-		connection.end(function(err) {
-			console.log("terminated mysql connection.")
-		})
-	})
-})
+// 		connection.end(function(err) {
+// 			console.log("terminated mysql connection.")
+// 		})
+// 	})
+// })
 
-app.post('/api/owners/:owner_id/team', (req, res) => {
+// app.post('/api/owners/:owner_id/team', (req, res) => {
 
-	const authz_str = req.headers.authorization
+// 	const authz_str = req.headers.authorization
 
-	console.log("authz_str: " + authz_str)
+// 	console.log("authz_str: " + authz_str)
 
-	const a = authz_str.split(" ")
+// 	const a = authz_str.split(" ")
 
-	const token = a[1]
+// 	const token = a[1]
 
-	console.log("token: " + token)
+// 	console.log("token: " + token)
 
-	if (token != process.env.API_KEY) {
-		res.sendStatus(403)
-		return
-	}
-	else {
+// 	if (token != process.env.API_KEY) {
+// 		res.sendStatus(403)
+// 		return
+// 	}
+// 	else {
 
-		console.log("the token is good!")
+// 		console.log("the token is good!")
 
-		const owner_id = req.params.owner_id
+// 		const owner_id = req.params.owner_id
 
-		console.dir(req.body)
+// 		console.dir(req.body)
 
-		var connection = dbconn.mysql_conn()
+// 		var connection = dbconn.mysql_conn()
 
-		const team_name = connection.escape(req.body.team_name)
-		const salary = req.body.salary
-		const bank = req.body.bank
+// 		const team_name = connection.escape(req.body.team_name)
+// 		const salary = req.body.salary
+// 		const bank = req.body.bank
 
-		let query = `INSERT INTO ownersXseasons_current (owner_id, team_name, salary, bank, season)`
+// 		let query = `INSERT INTO ownersXseasons_current (owner_id, team_name, salary, bank, season)`
 
-		query += ` VALUES (${owner_id}, ${team_name}, ${salary}, ${bank}, ${season})`
+// 		query += ` VALUES (${owner_id}, ${team_name}, ${salary}, ${bank}, ${season})`
 
-		console.log(query)
+// 		console.log(query)
 
-		connection.query(query, function (err, data) {
-			if (err) {
-				console.log(err)
-				res.json(err)
-				return
-			}
-			res.json({status: "ok"})
+// 		connection.query(query, function (err, data) {
+// 			if (err) {
+// 				console.log(err)
+// 				res.json(err)
+// 				return
+// 			}
+// 			res.json({status: "ok"})
 
-			connection.end(function(err) {
-				console.log("terminated mysql connection.")
-			})
-		})
-	}
-})
+// 			connection.end(function(err) {
+// 				console.log("terminated mysql connection.")
+// 			})
+// 		})
+// 	}
+// })
 
 /*************************************************/
 // Players
@@ -245,8 +243,6 @@ app.get('/api/players/:player_id/current', (req, res) => {
 	var player_id = req.params.player_id
 
 	console.log(player_id)
-
-	// const query = `SELECT * FROM player_x_season_detail WHERE player_id=${player_id} AND season=${season}`
 
 	const query = `SELECT * FROM player_x_season WHERE player_id=${player_id} AND season=${season}`
 
@@ -304,9 +300,7 @@ app.get('/api/seasons/current/players?*', (req, res) => {
 	const owner_id = req.query.owner_id
 	const pos = req.query.pos
 
-	let query = `SELECT * FROM player_x_season WHERE salary <= ${max_salary} AND pos = '${pos}' AND season = ${season} AND player_id NOT IN (SELECT player_id FROM owner_x_roster_detail WHERE owner_id = ${owner_id} AND season = ${season}) ORDER BY salary DESC`
-
-	// let query = `SELECT * FROM player_x_season_detail WHERE salary <= ${max_salary} AND pos = '${pos}' AND season = ${season} AND player_id NOT IN (SELECT player_id FROM owner_x_roster_detail WHERE owner_id = ${owner_id} AND season = ${season}) ORDER BY salary DESC`
+	let query = `SELECT * FROM player_x_season_detail WHERE salary <= ${max_salary} AND pos = '${pos}' AND season = ${season} AND player_id NOT IN (SELECT player_id FROM owner_x_roster_detail WHERE owner_id = ${owner_id} AND season = ${season}) ORDER BY salary DESC`
 
 	console.log(query)
 
